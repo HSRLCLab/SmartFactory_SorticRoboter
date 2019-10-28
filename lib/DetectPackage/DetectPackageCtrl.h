804c1a815118f2f5118c6bde286436564924d7b0
Philip Zellweger <p1zellwe@hsr.ch>
2019-10-28 09:22:21 +0100

    FSM Header to detect packages implemented

--- lib/DetectPackage/DetectPackageCtrl.h
+++ lib/DetectPackage/DetectPackageCtrl.h
@@ -1,7 +1,11 @@
+
 #ifndef DETECTPACKAGECTRL_H_
 #define DETECTPACKAGECTRL_H_
 
 #include <Arduino.h>
+#include "RfidCtrl.h"
+
+
 
 class DetectPackageCtrl
 {
@@ -9,8 +13,17 @@ class DetectPackageCtrl
 
     enum class Event
     {
-
+        CheckForPackage = 0,
+        PackageAvailableToSort = 1,
+        NoPackageAvailable = 2,
+        PackageReadyToSort = 3,
+        Error = 4,
+        Resume = 5,
+        NoEvent = 6
     };
+
+    
+    Package newPackage;
 
     DetectPackageCtrl();
 
@@ -20,28 +33,46 @@ class DetectPackageCtrl
 
     enum class State
     {
-
+        emptyState = 0,
+        checking = 1,
+        fullState = 2,
+        errorState = 3
     };
 
-    Event (SortPAckageCtrl::*doActionFPtr)(void) = nullptr;
+    State lastStateBevorError;  // holds the last State bevor error to resume after error
+    State currentState;         // holds the current state of the FSM
+    Event currentEvent;         // holds the current event of the FSM
+
+    RfidCtrl pRfidCtrl;
+
+
+    DetectPackageCtrl::Event (DetectPackageCtrl::*doActionFPtr)(void) = 0;
 
     void entryAction_emptyState();
 
-    DetectPackageCtrl::Event doAction_emptyState();
+    Event doAction_emptyState();
 
     void exitAction_emptyState();
     
-    void entryAction_checkForPackage();
+    void entryAction_checking();
 
-    DetectPackageCtrl::Event doAction_checkForPackage();
+    DetectPackageCtrl::Event doAction_checking();
 
-    void exitAction_checkForPackage();
+    void exitAction_checking();
 
     void entryAction_fullState();
 
     DetectPackageCtrl::Event doAction_fullState();
 
     void exitAction_fullState();
+
+    DetectPackageCtrl::Event doAction_errorState();
+
+    void exitAction_errorState();
+
+    String decodeEvent(Event event);
+
+    String decodeState(State state);
 }
 
 #endif
