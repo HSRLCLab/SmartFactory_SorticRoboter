94b58518b6cde968b67bd5e72765c3e9ac71fa70
Philip Zellweger <p1zellwe@hsr.ch>
2019-11-21 10:30:55 +0100

    Added timeconstants

--- lib/Configuration/MainConfiguration.h
+++ lib/Configuration/MainConfiguration.h
@@ -4,4 +4,7 @@
 #define RFIDDETECTOR_SDA         10
 #define RFIDDETECTOR_RST_PIN     9
 
+const unsigned int TIME_BETWEEN_PUBLISH = 300;
+const unsigned int SORTIC_WAITFOR_BOX_SECONDS = 5;   
+
 #endif
